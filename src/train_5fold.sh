c5dedc2939f6c4ba0f914ce46418405b6d91a21b
Nana2929 <b06102020@ntu.edu.tw>
2022-12-23 02:04:59 +0800

    mod: training script

--- src/train_5fold.sh
+++ src/train_5fold.sh
@@ -1,19 +1,25 @@
 device='0'
-save_dir='../save_dir/bigger_tp99/'
-
 # train 5fold
+save_dir='../save_dir/train_fake_bfo/'
+
 for fold_idx in {0..4}
 do
     echo === fold $fold_idx ===
     # train 49
     python3 train.py \
-        -c=config.json \
+        -c=config_trainfake.json \
         -d=$device \
         --fold_idx=$fold_idx \
-        --save_dir=$save_dir/fold$fold_idx
+        --save_dir=$save_dir/fold$fold_idx \
+        --seed=42
+    python3 test_1fold.py \
+        -c=$save_dir/fold0/config.json \
+        -f=$fold_idx \
+        -s=fold${fold_idx}_test \
+        -o=$save_dir
 done
-
 python3 test.py \
     --config=$save_dir/fold0/config.json \
     --output_dir=$save_dir \
-    --device=$device
+    --device=$device\
+    --suffix=5fold_ensemble\
